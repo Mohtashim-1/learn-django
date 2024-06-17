0a92799b04aafd65c97e3bcbfacf35eebbc2836e
Mohtashim <64360314+Mohtashim-1@users.noreply.github.com>
2024-06-17 23:35:38 +0500

    created views file

---
+++ chaiaurDjango/chaiaurDjango/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, home!")
+
+
+def about(request):
+    return HttpResponse("Hello, about!")
+
+
+def contact(request):
+    return HttpResponse("Hello, contact!")
